5dc6d6a9da6bd28e3f4a0de378e13fd6886590a4
greyhoundforty <ryantiffany@fastmail.com>
2024-05-11 07:16:02 -0500

    adding in flask template

--- app.py
+++ app.py
@@ -17,28 +17,22 @@ __author__ = 'ryantiffany'
 
 import socket
 from datetime import datetime 
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 """
 declare flask app
 """
 app = Flask(__name__)
-
-def get_host_info():
-    """
-    get hostname and ip info
-    """
-    hostname = socket.gethostname()
-    ip_address = socket.gethostbyname(hostname)
-    return hostname, ip_address
+Bootstrap(app)
 
 """declare route for app"""
 @app.route('/')
-def hello():
-    """display system info"""
-    hostname, ip = get_host_info()
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return f"Hello, World from a simple app<br>Hostname {hostname} is reporting the current time as: {current_time}<br>Current IP Address: {ip}<br>"
+def home():
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    message = "Welcome to the demo app!"
+    return render_template('index.html', hostname=hostname, ip=ip_address, message=message)
 
 """invoke script"""
 if __name__ == "__main__":
